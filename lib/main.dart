76e70e5f311f65f632ecbe101e597743bff69801
ujjwal <ujjwalmaity98@gmail.com>
2020-05-06 12:02:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
